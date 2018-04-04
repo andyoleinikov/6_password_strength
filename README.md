16b0f2b35eeb8c5662f7692339491553dce0bc83
anddyoleinikov <dyt321@ya.ru>
2018-04-04 20:51:50 +0300

    Made a script to calculate password strength

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+With this script you can verify password strength on a scale from 1 to 10 depending on length, used character types and if it is blacklisted. Password blacklist is top 25 worst passwords from [2017 Splash rating](https://13639-presscdn-0-80-pagely.netdna-ssl.com/wp-content/uploads/2017/12/Top-100-Worst-Passwords-of-2017a.pdf).
+
+# Quickstart
+
+Start the script in the command line and type password to check password strength.
+Or import get_password_strength function from password_strength.py file to use in your project.
+Default file path to password blacklist is 'passwords.txt', or it can be stated in the function call.
+
+### Example of import using interactive Python interpreter 
+
+```python
+>>> from password_strength import get_password_strength
+>>> get_password_strength('qwerty', 'blacklist_example.txt')
+1
+>>> get_password_strength('asdfghqwerty')
+5
+>>> get_password_strength('C00lH@ckeRr11')
+10
+```
+
+### Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python password_strength.py
+Input password: hello@world
+Password strength is: 6
+
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,64 @@
+import re
+import os
 
 
-def get_password_strength(password):
-    pass
+def load_blacklist(filepath):
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf-8') as text_file:
+            return text_file.read().split('\n')
+    return []
+
+
+def check_password_length(password):
+    password_strength = 0
+    if len(password) > 4:
+        password_strength += 1
+    if len(password) > 6:
+        password_strength += 1
+    if len(password) > 10:
+        password_strength += 1
+    if len(password) > 12:
+        password_strength += 1
+    return password_strength
+
+
+def check_char_types(password):
+    password_strength = 0
+    flag_uppercase = False
+    flag_lowercase = False
+    flag_digit = False
+    if re.search('[0-9]', password):
+        flag_digit = True
+        password_strength += 1
+    if re.search('[A-Z]', password):
+        flag_uppercase = True
+        password_strength += 1
+    if re.search('[a-z]', password):
+        flag_lowercase = True
+        password_strength += 1
+    if re.search('[$#@]', password):
+        password_strength += 1
+    if flag_digit and flag_lowercase and flag_uppercase:
+        password_strength += 1
+    return password_strength
+
+
+def get_password_strength(password, filepath_to_blacklist='passwords.txt'):
+    password_strength = 0
+    password_blacklist = load_blacklist(filepath_to_blacklist)
+    if password in password_blacklist:
+        return 1
+    if len(password) == 0:
+        return None
+    if len(set([letter for letter in password])) > 1:
+        password_strength += 1
+    password_strength += check_password_length(password)
+    password_strength += check_char_types(password)
+    return password_strength
 
 
 if __name__ == '__main__':
-    pass
+    password_for_test = input('Input password: ')
+    print(
+        'Password strength is:',
+        get_password_strength(password_for_test))
